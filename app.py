bfe168b57b334c06ccb4b97b1151746d54258771
ajubin <antoine.jubin@netatmo.com>
2018-05-24 15:35:25 +0200

    wrap model into a server

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import signal
+
+from flask import Flask, request
+from model.factorial import factorial
+
+os.environ['FLASK_ENV'] = 'development'
+app = Flask(__name__)
+
+signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    number = request.args.get("n")
+    if number:
+        number = int(number)
+        return "factorial({}) = {}".format(number, factorial(number))
+    return 'post a number'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
